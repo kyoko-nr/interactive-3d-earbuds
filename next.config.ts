b5a6196176b96b2c63971366ab0f4a948fc221ba
kyoko-nr <kyokokitagawa@me.com>
2025-04-14 21:57:37 +0900

    add next config for GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "interactive-3d-earbuds";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  distDir: "docs",
 };
 
 export default nextConfig;
