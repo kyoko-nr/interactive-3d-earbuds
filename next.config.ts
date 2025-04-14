645409857dbdde1299c44906a90a66fd46973485
kyoko-nr <kyokokitagawa@me.com>
2025-04-14 22:01:40 +0900

    delete unused config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "interactive-3d-earbuds";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
